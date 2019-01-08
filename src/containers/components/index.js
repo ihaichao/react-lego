f8ee8a3b701a4373b01cd68d4e864543cd4c625b
zhangzongzheng <zzzhaichao@gmail.com>
2019-01-09 00:13:34 +0800

    feat: drag

--- src/containers/components/index.js
+++ src/containers/components/index.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { Input } from 'antd';
 
 export default class Components extends Component {
-  state = {  }
+  handleDragStart = (e) => {
+    e.persist()
+    e.dataTransfer.setData('info', JSON.stringify({
+      componentName: e.target.getAttribute('data-name')
+    }))
+  }
+
   render() {
     return (
       <div className="components">
-        <div draggable="true">
+        <div draggable="true" onDragStart={this.handleDragStart} data-name="Input">
           <Input placeholder="input" />
         </div>
       </div>
--- src/containers/preview/index.js
+++ src/containers/preview/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 export default class Preview extends Component {
-  state = {  }
+  componentDidMount() {
+    this.handleListenDrag()
+  }
+
+  handleListenDrag() {
+    document.ondrag = (e) => {
+      const { left, right, top } = this.refs.preview.getBoundingClientRect()
+      if (e.clientX > left && e.clientX < right && e.clientY > top) {
+        // console.log('true')
+      }
+    }
+  }
+
+  handleDrop = (e) => {
+    console.log(e)
+    e.preventDefault()
+    // console.log(e.dataTransfer.getData('info'))
+  }
+  
   render() {
     return (
-      <div className="preview">
-        
-      </div>
+      <div className="preview" ref="preview" onDrop={this.handleDrop} style={{ height: '100%' }}></div>
     );
   }
 }
